78610e5f3ab5cf7ff7f81c8301d9a5cbc188d27b
Alex Ljubimow <35948176+aljubimow@users.noreply.github.com>
2018-02-20 20:01:00 -0800

    Add files via upload

---
+++ IDoDeclare.js
@@ -0,0 +1,25 @@
+//Example 1 - 4 Numbers
+var num= -1;
+console.log(num);
+var num= 1.24;
+console.log(num);
+var num= 100;
+console.log(num);
+var num= 50000;
+console.log(num);
+//Example 2 - 3 Strings
+var string1= "I do declare";
+console.log("The first string",string1,);
+var string2= "second";
+var string3= "final";
+console.log("Im working on the",string2,"before finishing with the",string3,"string");
+//Example 3 - 2 Booleans
+var bool= 10>9;
+console.log(bool);
+var bool= 55.59+45.313<99.28;
+console.log(bool);
+//Example 4 - Undefined Variable
+var test20;
+console.log(test20);
+
+
